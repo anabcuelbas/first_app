c54fe89f3b0baf89d45e229be0fca3265f543c1d
anabcuelbas <abcuelbas@gmail.com>
2020-09-04 13:01:59 -0300

    Adiciona listagem de tarefas

--- App.js
+++ App.js
@@ -1,27 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Open up App.js to start working on your app!")
+  const [enteredTask, setEnteredTask] = useState('')
+  const [dayTasks, setDayTasks] = useState([])
+
+  const taskInputHandler = (enteredText) => {
+    setEnteredTask(enteredText)
+  }
+
+  const addTaskHandler = () => {
+    setDayTasks(currentTasks => [
+      ...currentTasks, enteredTask
+    ])
+  }
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
         <TextInput 
-        placeholder="Course goal" 
-        style={styles.input}/>
-        <Button title="ADD"/>
+        placeholder="New task" 
+        style={styles.input}
+        onChangeText={taskInputHandler}
+        value={enteredTask}
+        />
+        <Button title="ADD" onPress={addTaskHandler}/>
       </View>
-      <View>
-
-      </View>
+      <ScrollView>
+        {dayTasks.map((task) =>
+          <View key={task} style={styles.listItem}>
+            <Text>{task}</Text>
+          </View>
+        )}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    padding: 50
+    padding: 50,
+    backgroundColor: '#f5f5f5',
+    flex: 1
   },
   inputContainer: {
     flexDirection: 'row',
@@ -33,5 +53,31 @@ const styles = StyleSheet.create({
     borderColor: 'black',
     borderWidth: 1,
     padding: 10
+  },
+  listItem: {
+    padding: 10,
+    marginVertical: 10,
+    backgroundColor: '#e0e0e0',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
+
+//Using FlatList instead of ScrollView
+
+/* const addTaskHandler = () => {
+  setDayTasks(currentTasks => [
+    ...currentTasks,
+    { key: Math.random().toString(), value: enteredTask }
+  ])
+} */
+
+{/* <FlatList
+      //keyExtractor={(item, index) => item.uid}
+      data={dayTasks} 
+      renderItem={itemData => {
+        <View style={styles.listItem}>
+          <Text>{itemData.item.value}</Text>
+        </View>
+      }}
+      /> */}
