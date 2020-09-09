d208663213fc5f2f8e573624f455f2239cb2dbeb
anabcuelbas <abcuelbas@gmail.com>
2020-09-09 11:05:36 -0300

    Adiciona estilo ao modal

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,15 +25,16 @@ const GoalInput = props => {
 
 const styles = StyleSheet.create({
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
         alignItems: 'center'
     },
     input: {
         width: '80%',
         borderColor: 'black',
         borderWidth: 1,
-        padding: 10
+        padding: 10,
+        marginBottom: 10
     }
 })
 
